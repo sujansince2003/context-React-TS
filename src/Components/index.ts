47b410b1198c8a94972d6043fef321e1b5125ea7
sujan khatri <sujansince2059@gmail.com>
2024-01-22 23:45:49 +0545

    import export component

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,4 +1,7 @@
 import Card from "./Card";
 import ThemeBtn from "./Themebtn";
+import Login from "./Login";
+import Profile from "./Profile";
 
-export {Card,ThemeBtn}
+
+export {Card,ThemeBtn,Login,Profile}
