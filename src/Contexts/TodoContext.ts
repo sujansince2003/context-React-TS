f4fd80abbd156dde4202e65846ee94657a7533f0
sujan khatri <sujansince2059@gmail.com>
2024-01-24 16:58:51 +0545

    defining type

--- src/Contexts/TodoContext.ts
+++ src/Contexts/TodoContext.ts
@@ -1,9 +1,29 @@
+
+
 import { createContext,useContext } from "react";
+
+
+export interface Todo {
+   id: number;
+    todo: string;
+    completed: boolean;
+  
+  }
 
 
 export const TodoContext=createContext(
     {
-
+todos:[
+    {
+        id:1,
+        todo:"Learn react context",
+        completed:false
+    }
+],
+addTodo:(todo:Todo)=>{},
+updateTodo:(id:number,todo:Todo)=>{},
+deleteTodo:(id:number)=>{},
+toggleTodo:(id:number)=>{},
     }
 )
 
@@ -13,3 +33,5 @@ export function useTodo()
 {
     return useContext(TodoContext)
 }
+
+// eslint-enable no-unused-vars
