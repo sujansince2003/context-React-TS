ca73068f3988f85494fefa2af1b25986c8a81c76
sujan khatri <sujansince2059@gmail.com>
2024-01-24 16:59:26 +0545

    adding context to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { TodoContextProvider, Todo } from "./Contexts";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [todos, setTodos] = useState<Todo[]>([]);
+  function addTodo(todo: Todo) {
+    setTodos((prev) => [...prev, todo]);
+  }
+  function toggleTodo() {}
+  function deleteTodo() {}
+  function updateTodo() {}
   return (
-    <>
+    <TodoContextProvider
+      value={{ todos, addTodo, toggleTodo, deleteTodo, updateTodo }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
@@ -19,7 +25,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </TodoContextProvider>
   );
 }
 
